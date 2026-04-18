c41e0fab8579def280fe7d188db814c6d677b72a
masihur <masihur96@gmail.com>
2026-04-18 14:37:41 +0600

    fix firebase provider for railway

--- src/firebase.provider.ts
+++ src/firebase.provider.ts
@@ -1,12 +1,18 @@
 import * as admin from 'firebase-admin';
 import { Provider } from '@nestjs/common';
-import * as serviceAccount from './config/firebase-admin.json';
 
 export const FirebaseProvider: Provider = {
   provide: 'FIREBASE_ADMIN',
   useFactory: () => {
+    if (admin.apps.length > 0) {
+      return admin.app();
+    }
+
+    const firebaseConfig = JSON.parse(process.env.FIREBASE_CONFIG!);
+    firebaseConfig.private_key = firebaseConfig.private_key.replace(/\\n/g, '\n');
+
     return admin.initializeApp({
-      credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
+      credential: admin.credential.cert(firebaseConfig),
     });
   },
 };
