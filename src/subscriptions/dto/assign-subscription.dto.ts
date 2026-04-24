58a5a43a1942feec238380fc4e6dfe3343247a5d
masihur <masihur96@gmail.com>
2026-04-25 05:19:17 +0600

    Filter User

--- src/subscriptions/dto/assign-subscription.dto.ts
+++ src/subscriptions/dto/assign-subscription.dto.ts
@@ -1,5 +1,5 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsString, IsNotEmpty, IsOptional, IsDateString, IsBoolean } from 'class-validator';
+import { IsString, IsNotEmpty, IsOptional, IsDate, IsBoolean } from 'class-validator';
 
 export class AssignSubscriptionDto {
   @ApiProperty({ example: 'SCHOOL_123', description: 'The unique ID of the school' })
@@ -13,12 +13,12 @@ export class AssignSubscriptionDto {
   pricingPlanId: string;
 
   @ApiProperty({ example: '2026-04-10T00:00:00Z', required: false })
-  @IsDateString()
+  @IsDate()
   @IsOptional()
   startDate?: Date;
 
   @ApiProperty({ example: '2027-04-10T00:00:00Z', required: false })
-  @IsDateString()
+  @IsDate()
   @IsOptional()
   endDate?: Date;
 
