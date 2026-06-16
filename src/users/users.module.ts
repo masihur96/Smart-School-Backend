b679ea72b050b026f55c62948984f03e40b87360
masihur <masihur96@gmail.com>
2026-06-16 06:20:03 +0600

    ClassIds and Section Ids

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,9 +3,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { User } from './entities/user.entity';
+import { ClassesModule } from '../classes/classes.module';
+import { SectionsModule } from '../sections/sections.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [TypeOrmModule.forFeature([User]), ClassesModule, SectionsModule],
   providers: [UsersService],
   controllers: [UsersController],
   exports: [UsersService],
