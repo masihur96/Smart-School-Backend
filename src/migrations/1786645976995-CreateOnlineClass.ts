9afefa0026434d1884123da9b5e946475c5f49d7
masihur <masihur96@gmail.com>
2026-08-14 00:35:53 +0600

    Phase 2

---
+++ src/migrations/1786645976995-CreateOnlineClass.ts
@@ -0,0 +1,14 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class CreateOnlineClass1786645976995 implements MigrationInterface {
+    name = 'CreateOnlineClass1786645976995'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`CREATE TABLE "online_class" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "title" character varying NOT NULL, "description" character varying, "meetLink" character varying NOT NULL, "date" TIMESTAMP WITH TIME ZONE NOT NULL, "startTime" character varying, "endTime" character varying, "hostId" character varying NOT NULL, "schoolId" character varying NOT NULL, "classId" character varying NOT NULL, "sectionId" character varying, "subjectId" character varying, "createdAt" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(), "updatedAt" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(), CONSTRAINT "PK_62707a82ed794cc3b4e63447129" PRIMARY KEY ("id"))`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`DROP TABLE "online_class"`);
+    }
+
+}
